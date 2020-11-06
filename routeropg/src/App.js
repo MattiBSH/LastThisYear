bed802d9b3169d90c4830e24062ae0fb17bee68e
MattiBSH <cph-mh829@cphbusiness.dk>
2020-11-06 10:59:19 +0100

    Nested

--- routeropg/src/App.js
+++ routeropg/src/App.js
@@ -1,91 +1,72 @@
-import React, {useState} from "react";
-import "./style2.css";
-import Header from "./Header";
+import React from "react";
+import ReactDOM from "react-dom";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
-  NavLink
+  useParams,
+  useRouteMatch
 } from "react-router-dom";
-
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
-export default function BasicExample() {
+function Topic() {
+  let { topicId } = useParams();
+  return (
+    <div>
+      <h3>{topicId}</h3>
+    </div>
+  );
+}
+function Topics() {
+  let { path, url } = useRouteMatch();
+  return (
+    <div>
+      <h2>Making it Nested</h2>
+      <ul>
+        <li>
+          <Link to={`${url}/SocialClimate`}>Social Climate</Link>
+        </li>
+        <li>
+          <Link to={`${url}/CallofDuty`}>Call of Duty</Link>
+        </li>
+        <li>
+          <Link to={`${url}/HarryPotter`}>Harry Potter</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+        <Route path={`${path}/:topicId`}>
+          <Topic />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+function App() {
   return (
     <Router>
       <div>
-        <Header/>
-
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
         <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <div className="content">
-
         <Switch>
           <Route exact path="/">
-            <Exercise1 />
+            <p>Home</p>
           </Route>
-          <Route path="/Exercise2">
-            <Exercise2 />
-          </Route>
-          <Route path="/Exercise3">
-            <Exercise3 />
+          <Route path="/topics">
+            <Topics />
           </Route>
         </Switch>
       </div>
-      </div>
     </Router>
-    
   );
 }
 
-// You can think of these components as "pages"
-// in your app.
-
-function Exercise1() {
-    // Declare a new state variable, which we'll call "count"
-    const [count, setCount] = useState(0);
-  
-  return (
-    <div>
-      <h2>Exercise1</h2>
-    
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
-}
-    
-
-
-function Exercise2() {
-  return (
-    <div>
-      <h2>Exercise2</h2>
-    </div>
-  );
-}
-
-function Exercise3() {
-  return (
-    <div>
-      <h2>Exercise3</h2>
-    </div>
-  );
-}
+export default App;
